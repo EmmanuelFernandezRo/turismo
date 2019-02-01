ce83dc70cf511a9c02423bafb11b631fe87b8fe4
Unknown <unknown@example.com>
2019-02-01 17:05:17 -0600

    Pantalla de reseteo de contraseña
    Se modifico el diseño de la pantalla de reseteo de contraseña

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,46 @@
 @extends('layouts.app')
 
+@section('Titulo-pag', 'Recuperar Contraseña')
+
+@section('body-class', 'signup-page')
+
 @section('content')
+<div class="header header-filter" style="background-image: url('{{asset ('img/city.jpg')}}'); background-size: cover; background-position: top center;">
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+            <div class="card card-signup">
+                <form class="form" method="POST"  action="{{ route('password.email') }}">
+                @csrf
+                    <div class="header header-primary text-center">
+                        <i class="fa fa-envelope-square fa-2x"></i><br><br>
+                        <h4>¡Recuperar Contraseña!</h4>
+                        <div class="social-line">
+                        <p class="text-divider"></p>
                         </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
+                    </div>
+                    <div class="content">
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="material-icons">email</i>
+                            </span>
+                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="footer text-center">
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Enviar Reseteo de Contraseña') }}
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
