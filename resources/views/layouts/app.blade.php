0d74234d4f8616dfa9321f8bba441911f7dd4dbe
Unknown <unknown@example.com>
2019-02-01 16:42:55 -0600

    Comentario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
 <body class="@yield('body-class')">
     <nav class="navbar navbar-transparent navbar-absolute">
     	<div class="container">
-        	<!-- Brand and toggle get grouped for better mobile display -->
+
         	<div class="navbar-header">
         		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example">
             		<span class="sr-only">Toggle navigation</span>
